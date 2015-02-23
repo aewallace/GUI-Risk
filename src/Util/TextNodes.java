328e433c520a12923d70300fc6793cef109877b9
aewallace <aew0024@tigermail.auburn.edu>
2015-02-22 19:16:09 -0600

    Hadn't added the backup TextNodes class － oops
    Added the TextNodes class that I’d used when making the portable
    version but hadn’t added to the repo until now… That should be one
    fewer error to encounter. Sorry!

---
+++ src/Util/TextNodes.java
@@ -0,0 +1,54 @@
+package Util;
+
+import java.lang.String;
+
+
+public class TextNodes{
+	public static final String nodes = new String(""
+		+"78 188 Alaska\n"
+		+"181 200 Northwest Territory\n"
+		+"225 273 Alberta\n"
+		+"345 289 Ontario\n"
+		+"469 292 Quebec\n"
+		+"577 107 Greenland\n"
+		+"232 361 Western US\n"
+		+"334 407 Eastern US\n"
+		+"228 488 Central America\n"
+		+"312 609 Venezuela\n"
+		+"325 733 Peru\n"
+		+"439 692 Brazil\n"
+		+"333 876 Argentina\n"
+		+"647 215 Iceland\n"
+		+"735 201 Scandinavia\n"
+		+"627 318 Great Britain\n"
+		+"709 345 Northern Europe\n"
+		+"606 460 Western Europe\n"
+		+"718 408 Southern Europe\n"
+		+"887 266 Ukraine\n"
+		+"628 618 North Africa\n"
+		+"772 547 Egypt\n"
+		+"759 743 Congo\n"
+		+"829 683 East Africa\n"
+		+"737 889 South Africa\n"
+		+"878 876 Madagascar\n"
+		+"877 518 Middle East\n"
+		+"958 364 Afghanistan\n"
+		+"1034 244 Ural\n"
+		+"1070 545 India\n"
+		+"1162 449 China\n"
+		+"1192 570 Siam\n"
+		+"1266 367 Mongolia\n"
+		+"1122 223 Siberia\n"
+		+"1239 281 Irkutsk\n"
+		+"1283 179 Yakutsk\n"
+		+"1432 196 Kamchatka\n"
+		+"1376 392 Japan\n"
+		+"1205 705 Indonesia\n"
+		+"1375 711 New Guinea\n"
+		+"1167 888 Western Australia\n"
+		+"1311 836 Eastern Australia\n");
+		
+	TextNodes()
+	{
+	}
+}
