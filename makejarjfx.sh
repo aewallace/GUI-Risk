7b7118346077996161e6d664fbcb558d851c8124
aewallace <aew0024@tigermail.auburn.edu>
2015-05-17 13:15:26 -0500

    Fix build script, add clean script
    Fixed: An obscure error regarding String instance variables declared as
    final occurs with JDK8 or JDK9, resulting in the secondary dialogs
    failing to open (essentially locking the user out of proceeding with
    their turn). To get around this, added the "-g" flag to the call to
    javac (the compilation stage) which enables all debugging information
    within the final code, which works around this. The cause of this issue
    is unknown, but does not occur with JDK7.
    
    Tweaked: text sent to the standard output directly from the script is
    indented, to provide a visible difference from messages printed by the
    called processes themselves.
    
    Tweaked: references to files are more specific, where required, to
    avoid copying useless/unnecessary files as resource files. Also added a
    few more comments and tweaked some stdout/echo text.
    
    Added: cleaning script, as a companion to the makejarjfx script. (Also
    removes the generated Jar file!); essentially attempts to return the
    folder to the original state, minus any extra files produced during the
    run of the application itself.

--- makejarjfx.sh
+++ makejarjfx.sh
@@ -5,31 +5,31 @@
 #in order to compile/archive (create a Jar) + run the JavaFX version of Risk from the command line.
 
 #rudimentary check for correct folder.
-echo "Checking location..."
-#if ! cd "src" ; then
+echo "   Validating script location..."
 if ! test /src/ ; then
-	echo "Not in root folder/could not find source folder! Script cannot work."
+	echo "***Not in root folder/could not find source folder! Script cannot work."
 else
-echo "Script may be in correct folder! Proceeding..."
-#cd .. #undo the "test"
+echo "   Script may be in correct folder! Proceeding..."
 
 #
 #clear the previous build folders, if it exists
-echo "Cleaning previous build files, if exist."
+echo "   Cleaning previous build files, if exist."
 rm -rf build
 
 #find all the source files in the project
 #and store them as a list in a text file
 find . -name "*.java" > srcFiles.txt
-echo "..."
+echo "   ..."
 
 #prepare a destination folder for Java CLASS files
 mkdir build
 
 #prepare the Java CLASS files from the source files, using the
 #list of files found in srcFiles.txt
-echo "Locating source files..."
-javac -d build @srcFiles.txt
+#flags: "-g": required to generate debugging info due to obscure bug
+# introduced by using "final String" variables in source code at select spots.
+echo "   Locating source files..."
+javac -g -d build @srcFiles.txt
 
 #create a manifest, in preparation for the creation of the final Jar.
 #This points to the class containing the main() method we desire.
@@ -39,21 +39,23 @@ echo Main-Class: Master.FXUIGameMaster >manifest.txt
 
 #copy extra resource files to the BUILD folder, to be added
 #to the Jar file. Output will be noisy.
-echo "Copying resources to build folder. (benign errors may occur)."
-cp **/*.jpg *.jpg **/*.txt *.txt build
+echo "   Copying resources to build folder. (benign errors may occur)."
+cp **/RiskBoard*.jpg RiskBoard*.jpg **/TextNodes.txt TextNodes.txt manifest.txt build
 
+#move into the build folder for proper compilation
 cd build
 
 #package everything into an easy-to-use Jar file.
 #includes additional resources such as the map & list of countries.
-echo "Attempting to create runnable Jar..."
+echo "   Attempting to create runnable Jar..."
 jar cvfm ../App.jar manifest.txt **/*.class *.txt *.jpg
 
+#return to the main project folder, where the jar should exist
 cd ..
 
 #run the application from the newly-created Jar file.
-echo "If successful, launching game!"
+echo "   If successful, launching game!"
 java -jar App.jar
 
-echo "Good-bye!"
+echo "   Good-bye!"
 fi
